47eec16a83b712c890d6594b95a10566a0cb9750
mreilly29 <mreilly18@jcu.edu>
2018-09-28 13:22:56 -0400

    added VirtualPet class

---
+++ VirtualPetShelter/VirtualPet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetShelter
+{
+    class VirtualPet
+    {
+        //properties
+        public string PetName { get; set; }
+        public string PetDescription { get; set; }
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+        public int Affection { get; set; }
+
+        //constructors
+        public VirtualPet(string name, string desciption)
+        {
+            PetName = name;
+            PetDescription = desciption;
+        }
+
+        public VirtualPet(int hunger, int thirst, int affection)
+        {
+            Hunger = hunger;
+            Thirst = thirst;
+            Affection = affection;
+        }
+    }
+}
